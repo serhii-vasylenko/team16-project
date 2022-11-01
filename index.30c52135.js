030ef78dc9132393f0345ebd528fa733f8512766
serhii-vasylenko <113785494+serhii-vasylenko@users.noreply.github.com>
2022-11-01 09:49:07 +0000

    Deploying to gh-pages from @ serhii-vasylenko/team16-project@cc9a4a85babdb8d5f618f014bb16442a5c0f501e 🚀

---
+++ index.30c52135.js
@@ -0,0 +1,2 @@
+var ref=document.querySelector(".header").getBoundingClientRect(),pageHeaderHeight=ref.height;document.body.style.paddingTop="".concat(pageHeaderHeight,"px"),console.log("".concat(pageHeaderHeight,"px"));
+//# sourceMappingURL=index.30c52135.js.map
